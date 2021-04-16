63857fd5315fadab06a02abcb8db16bf9c551cd0
Larfg <72176664+Larfg@users.noreply.github.com>
2021-04-16 12:05:05 -0500

    ciclo 0.1

---
+++ JewelQuest1/src/presentacion/JewelQuestGUI.java
@@ -0,0 +1,18 @@
+package presentacion;
+
+import javax.swing.*;
+
+public class JewelQuestGUI extends JFrame{
+
+    private JewelQuestGUI(){
+
+    }
+
+
+    public static void main(String[] args) {
+        JewelQuestGUI gui = new JewelQuestGUI();
+        gui.setTitle("JewelQuest");
+        gui.setVisible(true);
+    }
+
+}
